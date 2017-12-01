f2a03be8904e7bb4f5d4e8eeae2931154abbff5b
groot314 <groot314@gmail.com>
2017-12-01 12:26:18 -0800

    Fireballs now shoot center

--- Burgeon/Fireball.h
+++ Burgeon/Fireball.h
@@ -7,9 +7,8 @@ public:
 	FireBall(sf::Vector2f windowSize, sf::Vector2f pos = { 0,0 }, int shootDirection = -2, double speed = 2) :Sprite(getSprite()) {
 		this->windowSize = windowSize;
 
-		this->setPosition({pos.x-this->getTextureRect().width/2, pos.y - this->getTextureRect().height / 2 });
 		this->speed = speed;
-		this->shootDirection(shootDirection);
+		this->shootDirection(shootDirection,pos);
 	}
 
 	void setSpeed(double newSpeed)
@@ -49,28 +48,32 @@ private:
 	int xMovement;
 	int yMovement;
 
-	void shootDirection(int direction)
+	void shootDirection(int direction, sf::Vector2f pos)
 	{
 		if (direction == 1)//move up
 		{
+			this->setPosition({ pos.x - (this->getTextureRect().height / 2), pos.y - (this->getTextureRect().width / 2) });
 			this->xMovement = 0;
 			this->yMovement = -1;
 			this->setRotation(270);
 		}
 		else if (direction == 2)//move left
 		{
+			this->setPosition({ pos.x + (this->getTextureRect().width / 2), pos.y + (this->getTextureRect().height / 2) });
 			this->xMovement = -1;
 			this->yMovement = 0;
 			this->setRotation(180);
 		}
 		else if (direction == 3)//move right
 		{
+			this->setPosition({ pos.x - (this->getTextureRect().width / 2), pos.y - (this->getTextureRect().height / 2) });
 			this->xMovement = 1;
 			this->yMovement = 0;
 			this->setRotation(0);
 		}
 		else if (direction == 4)//move down
 		{
+			this->setPosition({ pos.x + (this->getTextureRect().height / 2), pos.y + (this->getTextureRect().width / 2) });
 			this->xMovement = 0;
 			this->yMovement = 1;
 			this->setRotation(90);
